c97e2e3e8fc9e4e210d035d96b196d4e1a7cb633
zhanglu <41885176+AdamZhang0124@users.noreply.github.com>
2021-08-06 17:17:43 +0800

    [dbg]Add log on ServiceProvisioning ConnectivityManagerImpl for debugging

--- src/platform/AMBD/ConnectivityManagerImpl.cpp
+++ src/platform/AMBD/ConnectivityManagerImpl.cpp
@@ -65,7 +65,158 @@ void ConnectivityManagerImpl::_OnPlatformEvent(const ChipDeviceEvent * event)
 #if CHIP_DEVICE_CONFIG_ENABLE_THREAD
     GenericConnectivityManagerImpl_Thread<ConnectivityManagerImpl>::_OnPlatformEvent(event);
 #endif
+}
+
+CHIP_ERROR ConnectivityManagerImpl::_SetWiFiStationMode(WiFiStationMode val)
+{
+    printf("%s %d ToDo\r\n", __func__,__LINE__);
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    VerifyOrExit(val != kWiFiStationMode_NotSupported, err = CHIP_ERROR_INVALID_ARGUMENT);
+
+    if (val != kWiFiStationMode_ApplicationControlled)
+    {
+        bool autoConnect = (val == kWiFiStationMode_Enabled);
+        //err              = Internal::ESP32Utils::SetAPMode(autoConnect);
+        SuccessOrExit(err);
+
+        SystemLayer.ScheduleWork(DriveStationState, NULL);
+    }
+
+    if (mWiFiStationMode != val)
+    {
+        ChipLogProgress(DeviceLayer, "WiFi station mode change: %s -> %s", WiFiStationModeToStr(mWiFiStationMode),
+                        WiFiStationModeToStr(val));
+    }
+
+    mWiFiStationMode = val;
+
+exit:
+    return err;
+}
+
+void ConnectivityManagerImpl::DriveStationState(::chip::System::Layer * aLayer, void * aAppState, ::CHIP_ERROR aError)
+{
+    sInstance.DriveStationState();
+}
+
+void ConnectivityManagerImpl::DriveStationState()
+{
+    bool stationConnected;
+#if 0
+    // Refresh the current station mode.  Specifically, this reads the ESP auto_connect flag,
+    // which determine whether the WiFi station mode is kWiFiStationMode_Enabled or
+    // kWiFiStationMode_Disabled.
+    GetWiFiStationMode();
+
+    // If the station interface is NOT under application control...
+    if (mWiFiStationMode != kWiFiStationMode_ApplicationControlled)
+    {
+        // Ensure that the ESP WiFi layer is started.
+        ReturnOnFailure(Internal::ESP32Utils::StartWiFiLayer());
+
+        // Ensure that station mode is enabled in the ESP WiFi layer.
+        ReturnOnFailure(Internal::ESP32Utils::EnableStationMode());
+    }
+
+    // Determine if the ESP WiFi layer thinks the station interface is currently connected.
+    ReturnOnFailure(Internal::ESP32Utils::IsStationConnected(stationConnected));
+
+    // If the station interface is currently connected ...
+    if (stationConnected)
+    {
+        // Advance the station state to Connected if it was previously NotConnected or
+        // a previously initiated connect attempt succeeded.
+        if (mWiFiStationState == kWiFiStationState_NotConnected || mWiFiStationState == kWiFiStationState_Connecting_Succeeded)
+        {
+            ChangeWiFiStationState(kWiFiStationState_Connected);
+            ChipLogProgress(DeviceLayer, "WiFi station interface connected");
+            mLastStationConnectFailTime = 0;
+            OnStationConnected();
+        }
+
+        // If the WiFi station interface is no longer enabled, or no longer provisioned,
+        // disconnect the station from the AP, unless the WiFi station mode is currently
+        // under application control.
+        if (mWiFiStationMode != kWiFiStationMode_ApplicationControlled &&
+            (mWiFiStationMode != kWiFiStationMode_Enabled || !IsWiFiStationProvisioned()))
+        {
+            ChipLogProgress(DeviceLayer, "Disconnecting WiFi station interface");
+            esp_err_t err = esp_wifi_disconnect();
+            if (err != ESP_OK)
+            {
+                ChipLogError(DeviceLayer, "esp_wifi_disconnect() failed: %s", esp_err_to_name(err));
+                return;
             }
+
+            ChangeWiFiStationState(kWiFiStationState_Disconnecting);
+        }
+    }
+
+    // Otherwise the station interface is NOT connected to an AP, so...
+    else
+    {
+        uint64_t now = System::Clock::GetMonotonicMilliseconds();
+
+        // Advance the station state to NotConnected if it was previously Connected or Disconnecting,
+        // or if a previous initiated connect attempt failed.
+        if (mWiFiStationState == kWiFiStationState_Connected || mWiFiStationState == kWiFiStationState_Disconnecting ||
+            mWiFiStationState == kWiFiStationState_Connecting_Failed)
+        {
+            WiFiStationState prevState = mWiFiStationState;
+            ChangeWiFiStationState(kWiFiStationState_NotConnected);
+            if (prevState != kWiFiStationState_Connecting_Failed)
+            {
+                ChipLogProgress(DeviceLayer, "WiFi station interface disconnected");
+                mLastStationConnectFailTime = 0;
+                OnStationDisconnected();
+            }
+            else
+            {
+                mLastStationConnectFailTime = now;
+            }
+        }
+
+        // If the WiFi station interface is now enabled and provisioned (and by implication,
+        // not presently under application control), AND the system is not in the process of
+        // scanning, then...
+        if (mWiFiStationMode == kWiFiStationMode_Enabled && IsWiFiStationProvisioned())
+        {
+            // Initiate a connection to the AP if we haven't done so before, or if enough
+            // time has passed since the last attempt.
+            if (mLastStationConnectFailTime == 0 || now >= mLastStationConnectFailTime + mWiFiStationReconnectIntervalMS)
+            {
+                ChipLogProgress(DeviceLayer, "Attempting to connect WiFi station interface");
+                esp_err_t err = esp_wifi_connect();
+                if (err != ESP_OK)
+                {
+                    ChipLogError(DeviceLayer, "esp_wifi_connect() failed: %s", esp_err_to_name(err));
+                    return;
+                }
+
+                ChangeWiFiStationState(kWiFiStationState_Connecting);
+            }
+
+            // Otherwise arrange another connection attempt at a suitable point in the future.
+            else
+            {
+                uint32_t timeToNextConnect = (uint32_t)((mLastStationConnectFailTime + mWiFiStationReconnectIntervalMS) - now);
+
+                ChipLogProgress(DeviceLayer, "Next WiFi station reconnect in %" PRIu32 " ms", timeToNextConnect);
+
+                ReturnOnFailure(SystemLayer.StartTimer(timeToNextConnect, DriveStationState, NULL));
+            }
+        }
+    }
+
+    ChipLogProgress(DeviceLayer, "Done driving station state, nothing else to do...");
+    // Kick-off any pending network scan that might have been deferred due to the activity
+    // of the WiFi station.
+#endif
+}
+
+
+
 
 } // namespace DeviceLayer
 } // namespace chip
