5aff5443670408b4df9cef5db60da24f9580a407
yangkunming99 <yangkunming99@gmail.com>
2021-08-10 01:54:24 +0800

    add connectivity port, still need to handle dhcp in UpdateInternetConnectivityState

--- src/platform/AMBD/CHIPDevicePlatformEvent.h
+++ src/platform/AMBD/CHIPDevicePlatformEvent.h
@@ -47,6 +47,7 @@ enum InternalPlatformSpecificEventTypes
     kCHIPoBLECCCWriteEvent,
     kCHIPoBLERXCharWriteEvent,
     kCHIPoBLETXCharWriteEvent,
+    kRtkWiFiStationConnectedEvent,
 };
 
 } // namespace DeviceEventType
