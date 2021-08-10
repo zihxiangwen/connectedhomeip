f30a2e82df6e987d0cde89d91758d31e50e360f5
yangkunming99 <yangkunming99@gmail.com>
2021-08-11 00:33:14 +0800

    increase tcp/udp connections, fix pyc timeout because dhcp takes too long, start mdns server when wifi is connected

--- src/app/server/Server.cpp
+++ src/app/server/Server.cpp
@@ -541,7 +541,8 @@ void InitServer(AppDelegate * delegate)
 
 // ESP32 and Mbed OS examples have a custom logic for enabling DNS-SD
 #if CHIP_DEVICE_CONFIG_ENABLE_MDNS && !CHIP_DEVICE_LAYER_TARGET_ESP32 && !CHIP_DEVICE_LAYER_TARGET_MBED
-    app::Mdns::StartServer();
+printf("%s %d todo use MACRO for AMBD\r\n");
+    //app::Mdns::StartServer();
 #endif
 
     gCallbacks.SetSessionMgr(&gSessions);
