9290d77e5d409b162f5d8de9fe897039b2909e78
step0035 <step0035@e.ntu.edu.sg>
2021-08-12 10:08:37 +0800

    Add AMBD macro for StartServer, add logs for ExchangeContext

--- src/protocols/secure_channel/CASESession.h
+++ src/protocols/secure_channel/CASESession.h
@@ -161,6 +161,7 @@ public:
     Messaging::ExchangeMessageDispatch * GetMessageDispatch(Messaging::ReliableMessageMgr * rmMgr,
                                                             SecureSessionMgr * sessionMgr) override
     {
+	printf("CASESession.h\r\n");
         return &mMessageDispatch;
     }
 
