5ca444373a8a93020e7c9f7941770c3d353aaf6f
step0035 <step0035@e.ntu.edu.sg>
2021-08-19 17:14:58 +0800

    Delete output files

--- examples/all-clusters-app/ambd/main/LEDWidget_lib_main.d
+++
@@ -1,6 +0,0 @@
-/home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/examples/all-clusters-app/ambd/main/LEDWidget_lib_main.oo: \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/examples/all-clusters-app/ambd/main/LEDWidget.cpp \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/examples/all-clusters-app/ambd/main/include/LEDWidget.h \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../component/common/api/platform/platform_stdlib.h \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../component/common/api/platform/platform_stdlib_rtl8710c.h \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../component/soc/realtek/8710c/cmsis/rtl8710c/include/basic_types.h
