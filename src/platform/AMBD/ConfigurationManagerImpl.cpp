c4e380ab686819940673c23f7df00d1022cda34b
zhanglu <41885176+AdamZhang0124@users.noreply.github.com>
2021-08-19 20:02:57 +0800

    Remove log and redundant codes

--- src/platform/AMBD/ConfigurationManagerImpl.cpp
+++ src/platform/AMBD/ConfigurationManagerImpl.cpp
@@ -81,7 +81,6 @@ CHIP_ERROR ConfigurationManagerImpl::_GetPrimaryWiFiMACAddress(uint8_t * buf)
 	sscanf(temp, "%02x:%02x:%02x:%02x:%02x:%02x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]);
 	for(i=0; i<ETH_ALEN; i++)
 		buf[i] = mac[i]&0xFF;
-	//printf("%s(): dev_mac="MAC_FMT"\n", __func__, MAC_ARG(dev_mac));
     return CHIP_NO_ERROR;
 }
 
