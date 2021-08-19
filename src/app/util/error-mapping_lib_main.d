56a70445bf52f67e3346714d2f7bb54e344e7c82
step0035 <step0035@e.ntu.edu.sg>
2021-08-19 17:14:58 +0800

    Delete output files

--- src/app/util/error-mapping_lib_main.d
+++
@@ -1,7 +0,0 @@
-/home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/src/app/util/error-mapping_lib_main.oo: \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/src/app/util/error-mapping.cpp \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/src/app/util/error-mapping.h \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/src/app/util/af-enums.h \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/src/protocols/interaction_model/Constants.h \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/src/protocols/Protocols.h \
- /home/zhanglu/CHIP/BLE_porting/BLE_B0728_gn_dev/ambz2_sdk_with_chip/project/realtek_amebaz2_v0_example/GCC-RELEASE/../../../third_party/connectedhomeip/src/lib/core/CHIPVendorIdentifiers.hpp
