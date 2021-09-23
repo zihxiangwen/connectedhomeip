6dfa80c61e9fcca6361a53c1f8c3c6d0eeec219e
hanksuu <71642969+hanksuu@users.noreply.github.com>
2021-09-23 17:05:14 +0800

    Fixed lighting-app build failed

--- examples/lighting-app/ambd/main/include/LEDWidget.h
+++ examples/lighting-app/ambd/main/include/LEDWidget.h
@@ -20,11 +20,11 @@
 #pragma once
 
 #include <platform_stdlib.h>
-#include "rtl8710c_pin_name.h"
 //#include "gpio_api.h"
 // TODO : Need to include gpio_api.h and fix compile error from hal_layer
 
 #ifdef CONFIG_PLATFORM_8710C
+#include "rtl8710c_pin_name.h"
 
 typedef struct hal_gpio_pin_adapter_s {
     uint8_t pin_name;               ///< chip pin name to locate the GPIO pin by port & pin index
